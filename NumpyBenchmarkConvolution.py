eef09b6ef90c5ee9c6b6b4aac12f874cf1738ace
Antoine Basset <antoine.basset@cnes.fr>
2024-07-08 17:40:14 +0200

    Use correlation instead of convolution

--- NumpyBenchmarkConvolution.py
+++ NumpyBenchmarkConvolution.py
@@ -3,6 +3,21 @@ import numpy as np
 from scipy import signal, ndimage
 import time
 
+
+def make_2d(side):
+    image = np.empty([side, side], dtype=int)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            image[i, j] = i+j
+    return image
+
+
+def print_2d(name, image):
+    print(f"{name}:")
+    print(f"  {image.shape[1]} x {image.shape[0]}")
+    print(f"  [{image[0,0]}, ... , {image[-1,-1]}]")
+
+
 if __name__ == "__main__":
 
     parser = ArgumentParser()
@@ -12,20 +27,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("Generating input and kernel...")
-    input = np.ones([args.image, args.image], dtype=int)
-    print(input)
-    kernel = np.ones([args.kernel, args.kernel], dtype=int)
-    print(kernel)
+    input = make_2d(args.image)
+    print_2d("input", input)
+    kernel = make_2d(args.kernel)
+    print_2d("kernel", kernel)
 
     method = args.extrapolation
     print("Filtering...")
     if method == None:
         start = time.perf_counter()
-        output = signal.convolve(input, kernel, mode="valid")
+        output = signal.correlate(input, kernel, mode="valid")
         stop = time.perf_counter()
     else:
         start = time.perf_counter()
-        output = ndimage.convolve(input, kernel, mode=method)
+        output = ndimage.correlate(input, kernel, mode=method)
         stop = time.perf_counter()
     print(f"  Done in {stop-start} s")
-    print(output)
+    print_2d("output", output)
