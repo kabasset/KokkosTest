1372b2b8779dd6f053ed068a023b3da4bc8c5b5f
Antoine Basset <antoine.basset@cnes.fr>
2024-07-16 11:17:22 +0200

    Review correlate()

--- include/Linx/Transforms/Correlation.h
+++ include/Linx/Transforms/Correlation.h
@@ -13,11 +13,24 @@
 
 namespace Linx {
 
+/**
+ * @brief Correlate two data containers
+ * 
+ * @param name The output name
+ * @param in The input container
+ * @param kernel The kernel container
+ * @param out The output container
+ * 
+ * Without extraplation, the output container is generally smaller than the input and kernel containers.
+ * In this case, the output extent along axis `i` is `in.extent(i) - kernel.extent(i) + 1`.
+ */
 template <typename TIn, typename TKernel, typename TOut>
 void correlate_to(const TIn& in, const TKernel& kernel, TOut& out)
 {
-  // FIXME copy kernel if not contiguous (see below)
-  auto in_data = in.data(); // FIXME is this in(0, 0)?
+  assert(kernel.container().span_is_contiguous() && "As of today, correlate_to() only accepts contiguous kernels.");
+  // FIXME copy kernel to Raster if not contiguous (see below)
+
+  auto in_data = in.data(); // FIXME This is not necessarily in(0...)
   auto kernel_data = kernel.data();
   auto kernel_size = kernel.size();
   Sequence<std::ptrdiff_t, -1> offsets("correlate_to(): offsets", kernel_size);
@@ -29,7 +42,7 @@ void correlate_to(const TIn& in, const TKernel& kernel, TOut& out)
       KOKKOS_LAMBDA(auto... is) {
         auto kernel_ptr = &kernel(is...);
         auto in_ptr = &in(is...);
-        auto index = kernel_ptr - kernel_data; // FIXME this assumes a contiguous span
+        auto index = kernel_ptr - kernel_data; // Assume a contiguous span
         values[index] = *kernel_ptr;
         offsets[index] = in_ptr - in_data;
       });
@@ -45,6 +58,9 @@ void correlate_to(const TIn& in, const TKernel& kernel, TOut& out)
       });
 }
 
+/**
+ * @copydoc correlate_to()
+ */
 template <typename TIn, typename TKernel>
 auto correlate(const std::string& name, const TIn& in, const TKernel& kernel)
 {
