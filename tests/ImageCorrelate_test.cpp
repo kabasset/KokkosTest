9403f2ae288eb00c91f535c0356147ec5e00847c
Antoine BASSET <antoine.basset@cnes.fr>
2024-09-05 14:17:25 +0000

    Fix and expand ImageCorrelate_test

--- tests/ImageCorrelate_test.cpp
+++ tests/ImageCorrelate_test.cpp
@@ -12,6 +12,29 @@ using Linx::ProgramContext;
 BOOST_TEST_GLOBAL_FIXTURE(ProgramContext);
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE);
 
+BOOST_AUTO_TEST_CASE(crop_to_test)
+{
+  const int width = 4;
+  const int height = 3;
+  const int kernel = 2;
+  using Image = Linx::Image<int, 2>;
+  Image a("a", width, height);
+  Image k("k", kernel, kernel);
+  Image b("b", width - kernel + 1, height - kernel + 1);
+  a.fill(1);
+  k.fill(1);
+  Kokkos::fence();
+
+  correlate_to(a, k, b);
+
+  const auto& b_on_host = Linx::on_host(b);
+  for (int j = 0; j < height - kernel; ++j) {
+    for (int i = 0; i < width - kernel; ++i) {
+      BOOST_TEST(b_on_host(i, j) == kernel * kernel);
+    }
+  }
+}
+
 BOOST_AUTO_TEST_CASE(crop_test)
 {
   const int width = 4;
@@ -26,9 +49,10 @@ BOOST_AUTO_TEST_CASE(crop_test)
 
   auto b = correlate("mean", a, k);
 
+  const auto& b_on_host = Linx::on_host(b);
   for (int j = 0; j < height - kernel; ++j) {
     for (int i = 0; i < width - kernel; ++i) {
-      BOOST_TEST(b(i, j) == kernel * kernel);
+      BOOST_TEST(b_on_host(i, j) == kernel * kernel);
     }
   }
 }
